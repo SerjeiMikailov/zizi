7fc88e1fbaef9b5bcfad2fa57daa31b25c3505b6
SerjeiMikailov <felipemaders8@gmail.com>
2023-11-20 23:31:20 -0300

    g

---
+++ src/Ls.hpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstring>
+#include <dirent.h>
+#include <iomanip>
+
+class FileOperations {
+public:
+    static void listDirectory(const char* path) {
+        DIR* dir = opendir(path);
+
+        if (dir == NULL) {
+            std::cout << "Error to open the directory" << std::endl;
+            return;
+        }
+
+        int maxNameLength = getMaxNameLength(dir, path);
+
+        rewinddir(dir);
+
+        printDirectoryContents(dir, maxNameLength);
+
+        closedir(dir);
+    }
+
+private:
+    static int getMaxNameLength(DIR* dir, const char* path) {
+        int maxNameLength = 0;
+        struct dirent* entry;
+
+        while ((entry = readdir(dir)) != NULL) {
+            if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+                char fullPath[PATH_MAX];
+                snprintf(fullPath, PATH_MAX, "%s/%s", path, entry->d_name);
+                int nameLength = strlen(fullPath);
+                if (nameLength > maxNameLength) {
+                    maxNameLength = nameLength;
+                }
+            }
+        }
+
+        return maxNameLength;
+    }
+
+    static void printDirectoryContents(DIR* dir, int maxNameLength) {
+        struct dirent* entry;
+
+        while ((entry = readdir(dir)) != NULL) {
+            if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+                std::cout << std::left << std::setw(maxNameLength + 2) << entry->d_name << std::endl;
+            }
+        }
+    }
+};
--- src/commands.cpp
+++ src/commands.cpp
@@ -1,22 +1,33 @@
 #include "App.hpp"
+#include "Ls.hpp"
+#include <unistd.h>
 
-void execute_command(const std::vector<std::string>& args) 
+void execute_command(const std::vector<std::string> &args)
 {
-    if (args.empty()) 
+    if (args.empty())
     {
         std::cout << "No command provided." << std::endl;
         return;
     }
 
-    if (args[0] == "zz" && args.size() == 2 && args[1] == "--v") 
+    if (args[0] == "zz" && args.size() == 2 && args[1] == "--v")
     {
         std::cout << "Zizi_Shell v1.0" << std::endl;
-    } else if (args[0] == "gg") 
+    }
+    else if (args[0] == "ls")
     {
-        std::cout << "gg" << std::endl;
-    } else 
+        char currentPath[PATH_MAX];
+        if (getcwd(currentPath, sizeof(currentPath)) != NULL)
+        {
+            FileOperations::listDirectory(currentPath);
+        }
+        else
+        {
+            std::cout << "Error getting current working directory" << std::endl;
+        }
+    }
+    else
     {
         std::cout << "Unknown command: " << args[0] << std::endl;
     }
 }
-
